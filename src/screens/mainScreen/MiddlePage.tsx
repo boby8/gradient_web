34d6a8cb7b5adffe0d00e9aa4b08db88d8b8c7da
Saurabh <c-saurabh.s@fit.zone>
2023-10-26 20:04:26 +0530

    added middle screen

--- src/screens/mainScreen/MiddlePage.tsx
+++ src/screens/mainScreen/MiddlePage.tsx
@@ -1,12 +1,43 @@
 import "../styles.css";
 import data from "../../assets/Union.png";
 import Circle from "../../assets/Circle.png";
+import img1 from "../../assets/Group1.png";
+import img2 from "../../assets/Group2.png";
+import img3 from "../../assets/Group3.png";
+import star from "../../assets/Star1.png";
 
 const MiddlePage = () => {
   interface brandIdentityArray {
     id: number;
     description: string;
   }
+
+  interface marketingList {
+    imgurl: string;
+    description: string;
+    title: string;
+  }
+
+  const marketingList: marketingList[] = [
+    {
+      imgurl: img1,
+      description:
+        "Our team of experts specializes in creating unique and effective designs ",
+      title: "Branding",
+    },
+    {
+      imgurl: img2,
+      description:
+        "Innovative and effective design solutions for business of our clients",
+      title: "Design",
+    },
+    {
+      imgurl: img3,
+      description:
+        "Our team of experienced designers and marketing professionals work closely ",
+      title: "Marketing",
+    },
+  ];
 
   const brandIdentityArray: brandIdentityArray[] = [
     {
@@ -111,6 +142,118 @@ const MiddlePage = () => {
           <p className="textWithGradient" style={{ fontSize: 54 }}>
             Help our clients achive <br /> their business objectives
           </p>
+        </div>
+      </div>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <div>
+            <p className="textWithGradient" style={{ fontSize: 12 }}>
+              Since 2006
+            </p>
+          </div>
+          <div>
+            <p>
+              Influenzilla is a leading design and marketing agency that
+              <br />
+              offers creative solutions to businesses of all sizes. Our team
+              <br />
+              of experts specializes in creating unique and effective designs
+              <br />
+              that help our clients stand out in a crowded market.
+              <br />
+            </p>
+
+            <p>
+              Our mission is to help our clients achieve their business
+              <br />
+              objectives through innovative and effective design solutions.
+              <br /> We believe that great design is more than just aesthetics.
+              It's
+              <br />
+              about creating a seamless user experience that engages and
+              <br />
+              delights your audience.
+            </p>
+          </div>
+          <div>
+            <p>
+              Influenzilla is a leading design and marketing agency that
+              <br />
+              offers creative solutions to businesses of all sizes. Our team
+              <br />
+              of experts specializes in creating unique and effective designs
+              <br />
+              that help our clients stand out in a crowded market.
+              <br />
+            </p>
+
+            <p>
+              Our mission is to help our clients achieve their business
+              <br />
+              objectives through innovative and effective design solutions.
+              <br /> We believe that great design is more than just aesthetics.
+              It's
+              <br />
+              about creating a seamless user experience that engages and
+              <br />
+              delights your audience.
+            </p>
+          </div>
+        </div>
+        <div
+          style={{ display: "flex", justifyContent: 'space-between', marginTop: "50px" }}
+        >
+          <div>
+            <img src={star} />
+          </div>
+          <div
+          style={{ display: "flex", justifyContent: "end", marginTop: "50px" }}
+        >
+          {marketingList.map((res, index) => (
+            <div
+              key={res.title}
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                border: "1px solid #ffffff ",
+                width: "30%",
+                justifyContent: "space-between",
+              }}
+            >
+              <div
+                style={{ 
+                  borderBottom: "1px solid #ffffff",
+                   padding: "10px", 
+                   height:"145px",
+                   display: "flex",
+                   justifyContent: "flex-start",
+                   alignItems: "center",
+                   paddingLeft: "51px",
+                  }}
+              >
+                <img src={res.imgurl} alt="" />
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  gap: "20px",
+                  // alignItems: "center",
+                  flexDirection: "column",
+                  padding: "36px 0 47px 51px",
+                }}
+              >
+                <p className="textWithGradient">{res.title}</p>
+                <p style={{
+                   fontSize: 12, 
+                  //  textAlign: "center", 
+                  margin: "0px",
+                   width: "50%" }}>
+                  {res.description}
+                </p>
+              </div>
+            </div>
+          ))}
+          </div>
         </div>
       </div>
     </div>
